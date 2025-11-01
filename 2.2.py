656161efa8f812893d9b8a56391bcbc78f9780a1
Muchabzz <kamil26082005@wp.pl>
2025-11-01 20:17:51 +0100

    first commit

---
+++ 2.2.py
@@ -0,0 +1,17 @@
+sum1 = 3 * 2 + 1
+print("sum1 = ", sum1)
+
+sum2 = 5 + 10 * 5
+print("sum2 = ", sum2)
+
+sum3 = 4 + 4 / 2 ** 2
+print("sum3 = ", sum3)
+
+sum4 = 4 % 3 % 2 % 1
+print("sum4 = ", sum4)
+
+sum5 = 1 + 2 % 3 ** 4 * 5
+print("sum5 = ", sum5)
+
+sum6 = True != False
+print("sum6 = ", sum6)
