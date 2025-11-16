5a8e235cdfabd292eae3f3e35c47f9999270b0c4
Muchabzz <kamil26082005@wp.pl>
2025-11-16 09:19:10 +0100

    1

--- 6.1.py
+++ 6.1.py
@@ -10,4 +10,4 @@ characters_in_surname = len(surname)
 characters_in_full_name = len(full_name)
 print(f'Your name has {characters_in_name} characters')
 print(f'Your surname has {characters_in_surname} characters')
-print(f'Your full name has {characters_in_full_name}') # with a space between name and surname
+print(f'Your full name has {characters_in_full_name}') # with a space between name and surname 
